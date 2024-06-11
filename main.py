b418f1f5a5f5582ecbf86e593f96303f3fd77630
Christoffer Jansson <christofferjansson1@gmail.com>
2024-06-11 16:38:15 +0200

    class init

--- main.py
+++ main.py
@@ -1,34 +1,89 @@
 import random
 
-cards = []
-suits = ["spades", "clubs", "hearts", "diamonds"]
-ranks = ["Ace", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
 
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
 
-
-def shuffle():
-    random.shuffle(cards)
+    def __str__(self):
+        return f"{self.rank["rank"]} of {self.suit}"
 
 
-def deal(number):
-    cards_dealt = []
-    for x in range(number):
-        card = cards.pop()
-        cards_dealt.append(card)
-    return cards_dealt
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suits = ["spades", "clubs", "hearts", "diamonds"]
+        ranks = [
+            {"rank": "A", "value": 11},
+            {"rank": "2", "value": 2},
+            {"rank": "3", "value": 3},
+            {"rank": "4", "value": 4},
+            {"rank": "5", "value": 5},
+            {"rank": "6", "value": 6},
+            {"rank": "7", "value": 7},
+            {"rank": "8", "value": 8},
+            {"rank": "9", "value": 9},
+            {"rank": "10", "value": 10},
+            {"rank": "Jack", "value": 10},
+            {"rank": "Queen", "value": 10},
+            {"rank": "King", "value": 10}
+        ]
+
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(suit, rank))
+
+    def shuffle(self):
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)
+
+    def deal(self, number):
+        cards_dealt = []
+        for x in range(number):
+            if len(self.cards) > 0:
+                card = self.cards.pop()
+                cards_dealt.append(card)
+        return cards_dealt
 
 
-shuffle()
-cards_dealt = deal(2)
-card = cards_dealt[0]
-rank = card[1]
+class Hand:
+    def __init__(self, dealer=False):
+        self.cards = []
+        self.value = 0
+        self.dealer = dealer
 
-if rank == "Ace":
-    value = 11
-elif rank == "Jack":
-    value = 10
+    def add_card(self, card_list):
+        self.cards.extend(card_list)
 
-print(card)
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+
+        for card in self.cards:
+            card_value = int(card.rank["value"])
+            self.value += card_value
+            if card.rank["rank"] == "Ace":
+                has_ace = True
+
+        if has_ace and self.value > 21:
+            self.value -= 10
+
+    def get_value(self):
+        self.calculate_value()
+        return self.value
+
+    def is_blackjack(self):
+        return self.get_value() == 21
+
+    def display(self):
+        print(f'''{"Dealer's" if self.dealer else "Your"} hand: ''')
+        for 
+
+
+deck = Deck()
+deck.shuffle()
+
+hand = Hand()
+hand.add_card(deck.deal(2))
+print(hand.cards[0], hand.cards[1])
