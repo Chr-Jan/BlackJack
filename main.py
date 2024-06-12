442b7851eae6cbb9c6e61586689705f8c5a01206
Christoffer Jansson <christofferjansson1@gmail.com>
2024-06-12 07:16:09 +0200

    improve & comments

--- main.py
+++ main.py
@@ -2,20 +2,36 @@ import random
 
 
 class Card:
+    """
+    Represents a single playing card with a suit and a rank.
+    """
+
     def __init__(self, suit, rank):
+        """
+        Initialize the card with a suit and rank.
+        """
         self.suit = suit
         self.rank = rank
 
     def __str__(self):
-        return f"{self.rank["rank"]} of {self.suit}"
+        """
+        Return a string representation of the card.
+        """
+        return f"{self.rank['rank']} of {self.suit}"
 
 
 class Deck:
+    """
+    Represents a deck of 52 playing cards.
+    """
+
     def __init__(self):
-        self.cards = []
+        """
+        Initialize the deck with 52 cards.
+        """
         suits = ["spades", "clubs", "hearts", "diamonds"]
         ranks = [
-            {"rank": "A", "value": 11},
+            {"rank": "Ace", "value": 11},
             {"rank": "2", "value": 2},
             {"rank": "3", "value": 3},
             {"rank": "4", "value": 4},
@@ -29,159 +45,201 @@ class Deck:
             {"rank": "Queen", "value": 10},
             {"rank": "King", "value": 10}
         ]
-
-        for suit in suits:
-            for rank in ranks:
-                self.cards.append(Card(suit, rank))
+        self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
 
     def shuffle(self):
-        if len(self.cards) > 1:
-            random.shuffle(self.cards)
+        """
+        Shuffle the deck of cards.
+        """
+        random.shuffle(self.cards)
 
     def deal(self, number):
-        cards_dealt = []
-        for x in range(number):
-            if len(self.cards) > 0:
-                card = self.cards.pop()
-                cards_dealt.append(card)
-        return cards_dealt
+        """
+        Deal a number of cards from the deck.
+        """
+        return [self.cards.pop() for _ in range(number)]
 
 
 class Hand:
+    """
+    Represents a hand of cards held by a player or dealer.
+    """
+
     def __init__(self, dealer=False):
+        """
+        Initialize the hand, optionally as a dealer's hand.
+        """
         self.cards = []
-        self.value = 0
         self.dealer = dealer
 
     def add_card(self, card_list):
+        """
+        Add a list of cards to the hand.
+        """
         self.cards.extend(card_list)
 
-    def calculate_value(self):
-        self.value = 0
-        has_ace = False
-
-        for card in self.cards:
-            card_value = int(card.rank["value"])
-            self.value += card_value
-            if card.rank["rank"] == "Ace":
-                has_ace = True
-
-        if has_ace and self.value > 21:
-            self.value -= 10
-
     def get_value(self):
-        self.calculate_value()
-        return self.value
+        """
+        Calculate and get the total value of the hand, accounting for aces.
+        """
+        value = sum(card.rank["value"] for card in self.cards)
+        num_aces = sum(1 for card in self.cards if card.rank["rank"] == "Ace")
+
+        while value > 21 and num_aces:
+            value -= 10
+            num_aces -= 1
+
+        return value
 
     def is_blackjack(self):
+        """
+        Check if the hand is a blackjack (total value of 21 with two cards).
+        """
         return self.get_value() == 21
 
     def display(self, show_all_dealer_cards=False):
-        print(f'''{"Dealer's" if self.dealer else "Your"} hand: ''')
-        for index, card in enumerate(self.cards):
-            if index == 0 and self.dealer and not show_all_dealer_cards and not self.is_blackjack():
-                print("hidden")
-            else:
+        """
+        Display the cards in the hand, hiding the dealer's first card if applicable.
+        """
+        print(f"{'Dealer' if self.dealer else 'Your'} hand:")
+        if self.dealer and not show_all_dealer_cards:
+            print("hidden")
+            print(self.cards[1])
+        else:
+            for card in self.cards:
                 print(card)
-
-        if not self.dealer:
-            print("Value:", self.get_value())
+            if not self.dealer:
+                print("Value:", self.get_value())
         print()
 
 
 class Game:
+    """
+    Represents a game of Blackjack.
+    """
+
     def play(self):
-        game_number = 0
-        games_to_play = 0
+        """
+        Play the specified number of games of Blackjack.
+        """
+        games_to_play = self.get_number_of_games()
 
-        while games_to_play <= 0:
-            try:
-                games_to_play = int(input("How many games do you want to play?"))
-            except:
-                print("You most enter a number.")
-
-        while game_number < games_to_play:
-            game_number += 1
-
+        for game_number in range(1, games_to_play + 1):
             deck = Deck()
             deck.shuffle()
 
             player_hand = Hand()
             dealer_hand = Hand(dealer=True)
 
-            for i in range(2):
-                player_hand.add_card(deck.deal(1))
-                dealer_hand.add_card(deck.deal(1))
+            player_hand.add_card(deck.deal(2))
+            dealer_hand.add_card(deck.deal(2))
 
-            print()
-            print("*" * 30)
-            print(f"Game {game_number} of {games_to_play}")
-            print("*" * 30)
+            print(f"\n{'*' * 30}\nGame {game_number} of {games_to_play}\n{'*' * 30}")
             player_hand.display()
             dealer_hand.display()
 
             if self.check_winner(player_hand, dealer_hand):
                 continue
 
-            choice = ""
-            while player_hand.get_value() < 21 and choice not in ["s", "stand"]:
-                choice = input("Please choose 'Hit' or 'Stand': ").lower()
-                print()
-                while choice not in ["h", "s", "hit", "stand"]:
-                    choice = input("Please enter 'Hit' (H) or 'Stand' (S) ").lower()
-                    print()
-                if choice in ["hit", "h"]:
-                    player_hand.add_card(deck.deal(1))
-                    player_hand.display()
-
+            self.player_turn(player_hand, deck)
             if self.check_winner(player_hand, dealer_hand):
                 continue
 
-            player_hand_value = player_hand.get_value()
-            dealer_hand_value = dealer_hand.get_value()
-            while dealer_hand_value < 17:
-                dealer_hand.add_card(deck.deal(1))
-                dealer_hand_value = dealer_hand.get_value()
+            self.dealer_turn(dealer_hand, deck)
+            self.show_final_results(player_hand, dealer_hand)
 
-            dealer_hand.display(show_all_dealer_cards=True)
+    @staticmethod
+    def get_number_of_games():
+        """
+        Get the number of games to play from the user.
 
-            if self.check_winner(player_hand, dealer_hand):
-                continue
+        Static Method: This method does not depend on any specific instance of the Game class.
+        It is used to encapsulate functionality related to getting input from the user.
+        """
+        while True:
+            try:
+                games_to_play = int(input("How many games do you want to play? "))
+                if games_to_play > 0:
+                    return games_to_play
+                else:
+                    print("You must enter a positive number.")
+            except ValueError:
+                print("You must enter a number.")
 
-            print("Final Results")
-            print("Your hand: ", player_hand_value)
-            print("Dealer hand: ", dealer_hand_value)
+    @staticmethod
+    def player_turn(player_hand, deck):
+        """
+        Handle the player's turn.
 
-            self.check_winner(player_hand, dealer_hand, True)
-
-    print("\nThanks for playing")
-
-    def check_winner(self, player_hand, dealer_hand, game_over=False):
-        if not game_over:
-            if player_hand.get_value() > 21:
-                print("You lost. Dealer wins.")
-                return True
-            elif dealer_hand.get_value() > 21:
-                print("Dealer lost. You win.")
-                return True
-            elif dealer_hand.is_blackjack() and player_hand.is_blackjack():
-                print("Both players have blackjack, it is a tie.")
-                return True
-            elif player_hand.is_blackjack():
-                print("You have blackjack. You win")
-                return True
-            elif dealer_hand.is_blackjack():
-                print("Dealer have blackjack. You loose")
-                return True
-        else:
-            if player_hand.get_value() > dealer_hand.get_value():
-                print("You win")
-            elif player_hand.get_value() == dealer_hand.get_value():
-                print("Tie")
+        Static Method: This method operates on the parameters passed to it and does not require access to
+        any specific instance variables of the Game class. It encapsulates the functionality of the player's turn.
+        """
+        while player_hand.get_value() < 21:
+            choice = input("Please choose 'Hit' or 'Stand': ").lower()
+            if choice in ["stand", "s"]:
+                break
+            elif choice in ["hit", "h"]:
+                player_hand.add_card(deck.deal(1))
+                player_hand.display()
             else:
-                print("Dealer wins")
+                print("Please enter 'Hit' (H) or 'Stand' (S).")
+
+    @staticmethod
+    def dealer_turn(dealer_hand, deck):
+        """
+         Handle the dealer's turn.
+
+         Static Method: Similar to player_turn, this method operates on the parameters passed to it and does not
+         require access to any specific instance variables of the Game class. It encapsulates the functionality
+         of the dealer's turn.
+         """
+        while dealer_hand.get_value() < 17:
+            dealer_hand.add_card(deck.deal(1))
+        dealer_hand.display(show_all_dealer_cards=True)
+
+    @staticmethod
+    def check_winner(player_hand, dealer_hand, game_over=False):
+        """
+        Determine the winner of the game.
+
+        Static Method: This method operates on the parameters passed to it and does not require access to any
+        specific instance variables of the Game class. It encapsulates the functionality of determining the winner.
+        """
+        player_value = player_hand.get_value()
+        dealer_value = dealer_hand.get_value()
+
+        if player_value > 21:
+            print("You busted! Dealer wins.")
+            return True
+        elif dealer_value > 21:
+            print("Dealer busted! You win.")
+            return True
+        elif game_over:
+            if player_value > dealer_value:
+                print("You win!")
+            elif player_value < dealer_value:
+                print("Dealer wins!")
+            else:
+                print("It's a tie!")
             return True
         return False
 
-g = Game()
-g.play()
+    @staticmethod
+    def show_final_results(player_hand, dealer_hand):
+        """
+        Display the final results of the game.
+
+        Static Method: Similar to check_winner, this method operates on the parameters passed to it and does not
+        require access to any specific instance variables of the Game class. It encapsulates the functionality
+        of displaying the final results of the game.
+        """
+        print("Final Results")
+        print("Your hand:", player_hand.get_value())
+        print("Dealer's hand:", dealer_hand.get_value())
+        Game.check_winner(player_hand, dealer_hand, game_over=True)
+
+
+# Create a game instance and start the game
+if __name__ == "__main__":
+    g = Game()
+    g.play()
